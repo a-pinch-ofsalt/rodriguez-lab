bdc0239a173016471bb8dae4916609c3ee93293e
Franklin Hiciano <fhiciano5@gmail.com>
2025-05-14 18:27:36 -0400

    pggb works on Minerva

--- results/2025-05-13_trying_pggb/run.sh
+++ results/2025-05-13_trying_pggb/run.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
-#BSUB -J pggb
+#BSUB -P acc_oscarlr
+#BSUB -q premium
 #BSUB -n 4
-#BSUB -R "rusage[mem=8000]"
-#BSUB -W 4:00
-#BSUB -o pggb.out
-#BSUB -e pggb.err
-#BSUB -P testing_pggb
+#BSUB -W 00:05
+#BSUB -o pggb_yes.out
 
-git clone https://github.com/ekg/guix-genomics
-cd guix-genomics
-GUIX_PACKAGE_PATH=. guix package -i pggb
+
+# git clone https://github.com/ekg/guix-genomics
+# cd guix-genomics
+# echo HOWDY
+# pwd
+# GUIX_PACKAGE_PATH=. guix package -i pggb
+
+
+
+
+module load anaconda3 samtools
+# source 
+
+# function create_env {
+#     conda init
+#     conda create -n pggb-env -c bioconda -c conda-forge odgi smoothxg wfmash=0.13.1 -y
+#     conda activate pggb-env
+#     conda install -c bioconda pggb -y
+# }
+
+# create_env
+
+# conda activate pggb-env
+
 
 
 data_folder="/sc/arion/work/hiciaf01/data/2025-05-12_ref_genomes"
@@ -18,4 +37,5 @@ small_data_folder="/sc/arion/work/hiciaf01/results/2025-05-12_extracting_igh"
 cat ${small_data_folder}/hg19_region.fa ${small_data_folder}/hg38_region.fa > combined.fa
 samtools faidx combined.fa
 
+echo OOGABOOGA
 pggb -i combined.fa -o output -s 5000 -p 95 -n 2 -t 1
