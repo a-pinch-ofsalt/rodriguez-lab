6a84fa620680430ef3909817e6e772b5375c40ec
Franklin Hiciano <fhiciano5@gmail.com>
2025-05-21 08:42:19 -0400

    fully refactored run.sh

--- results/2025-05-14_pggb_runner_beta/run.sh
+++ results/2025-05-14_pggb_runner_beta/run.sh
@@ -1,75 +1,39 @@
 #!/bin/bash
 
-hg38_file_general_match=$(ls *hg38.fa* 2>/dev/null | head -n 1)
-hg19_file_general_match=$(ls *hg19.fa* 2>/dev/null | head -n 1)
-
-if [ ! -n "${hg38_file_general_match}" ]; then
-	echo "Downloading hg38.fa.gz"
-	wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
-fi
-
-if [ ! -n "${hg19_file_general_match}" ]; then
-	echo "Downloading hg19.fa.gz"
-	wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
-fi
-
-if [ ! -f hg38.fa ]; then
-	echo "Unzipping hg38.fa.gz"
-	gunzip hg38.fa.gz	
-fi
-
-if [ ! -f hg19.fa ]; then
-	echo "Unzipping hg19.fa.gz"
-	gunzip hg19.fa.gz
-fi
-
-function minigraph {
-    
-    git clone https://github.com/lh3/minigraph
-
-    cd minigraph && make
-
-    ./minigraph -cxggs -t16 ../hg19_region.fa ../hg38_region.fa > minigraph_test.gfa
+function get_ighv_locus {
+    if [ ! -n "$(ls *hg38.fa* 2>/dev/null | head -n 1)" ]; then
+        wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz
+    fi
+    if [ ! -n "$(ls *hg19.fa* 2>/dev/null | head -n 1)" ]; then
+        wget https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz
+    fi
+    gunzip hg38.fa.gz
+    gunzip hg19.fa.gz
+    module load samtools
+    samtools faidx hg38.fa chr14:105039756-107043718 > hg38_region.fa
+    samtools faidx hg19.fa chr14:105505611-107349540 > hg19_region.fa
 }
-function run_pggb {
-    
-    module load anaconda3 samtools
 
+function create_pangenome_env {
+    module load anaconda3
+    conda init
+    conda create -n pangenome-env -c bioconda -c conda-forge pggb odgi smoothxg wfmash=0.13.1 -y
 
-    function create_env {
-	conda init
-	conda create -n pggb-env -c bioconda -c conda-forge odgi smoothxg wfmash=0.13.1 -y
-	conda activate pggb-env
-	conda install -c bioconda pggb -y
-    }
-    
-    create_env
-    
-    conda activate pggb-env
-    
+    # function minigraph_create_pangenome_graph {
+    #     git clone https://github.com/lh3/minigraph
+    #     cd minigraph && make
+    # }
+}
+function create_pangenome_graph {
+    conda activate pangenome-env
     cat hg19_region.fa hg38_region.fa > combined.fa
     samtools faidx combined.fa
-    
     pggb -i combined.fa -o output -s 5000 -p 95 -n 2 -t 1
-}
-function cut_seqs {
-    if ! command -v samtools &> /dev/null; then
-	echo "Installing samtools..."
-	module load samtools
-    else
-	echo "Samtools already installed..."
-    fi
-    
-    hg38_region="chr14:105039756-107043718"
-    hg19_region="chr14:105505611-107349540"
-    
-    samtools faidx hg38.fa ${hg38_region} > hg38_region.fa
-    samtools faidx hg19.fa ${hg19_region} > hg19_region.fa
-}
 
+    # ./minigraph -cxggs -t16 ../hg19_region.fa ../hg38_region.fa > minigraph_test.gfa
+}
 # download hg_gencode_v47.bed and hg19_vega.bed from https://genome.ucsc.edu/cgi-bin/hgTables
-function backup_genes {
-
+function backup_ighv {
     cp hg38_gencode_v47.bed hg38_gencode_v47_backup.bed
     cp hg19_vega.bed hg19_vega_backup.bed
 }
@@ -80,7 +44,7 @@ function filter_ighv_genes {
     grep 'IGHV' hg19_vega_backup.bed | grep -v 'alt' > temp_hg19.bed
     mv temp_hg19.bed hg19_vega_backup.bed
 }
-function subtract_start_position_and_set_proper_labels {
+function subtract_start_position_and_set_proper_labels_for_odgi {
     awk -v start_pos=105039755 '{ $1 = "chr14:105039756-107043718"; $2 -= start_pos; $3 -= start_pos; print }' hg38_gencode_v47_backup.bed > temp_hg38.bed
     mv temp_hg38.bed hg38_gencode_v47_backup.bed
     awk -v start_pos=105505611 '{ $1 = "chr14:105505611-107349540"; $2 -= start_pos; $3 -= start_pos; print }' hg19_vega_backup.bed > temp_hg19.bed
@@ -97,9 +61,18 @@ function inject_genes {
 function get_genes_in_SVs {
     odgi position -i pggb.og -p IGHV3-64D
 }
-
-#backup_genes
-#filter_ighv_genes
-#subtract_start_position_and_set_proper_labels
+download_genomes
+create_pangenome_env
+create_pangenome_graph
+backup_ighv
+filter_ighv_genes
+subtract_start_position_and_set_proper_labels_for_odgi
 inject_genes
 
+
+Different writing styles greatly affect the quality of the generated text.
+The quality of the generated text is greatly affected by different writing styles.
+Writing styles have a significant impact on the quality of the generated text.
+How you write can greatly influence the quality of the text that is generated.
+Generated text is sensitively affected by the writing style.
+Writing style affects generated text quality.
