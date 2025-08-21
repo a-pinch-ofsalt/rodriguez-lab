c272c33e86d194c477d4cd15f95a75d56c8a8d91
Franklin Hiciano <105998129+a-pinch-ofsalt@users.noreply.github.com>
2025-08-21 12:41:09 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='franklin-lab-notebook',
+    version='0.1.0',
+    description='Data & Results from Franklin's lab notebook.',
+    author='Franklin Hiciano',
+    author_email='fhiciano5@gmail.com',
+    url='https://github.com/a-pinch-ofsalt/rodriguez-lab',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'biopython',
+        'receptor-utils'
+    ],
+    python_requires='>=3.8',
+)
